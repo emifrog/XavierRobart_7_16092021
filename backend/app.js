319d733432839f7d33f46f157be9aa8e3e6f3e73
xavius06 <xav.robart@gmail.com>
2021-09-17 16:09:47 +0200

    modif app.js

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser'); // pour importer le package body parser
+const bodyParser = require('body-parser'); // aide à analyser la demande et à créer l' req.bodyobjet
 
 const app = express();
+const cors = require('cors')  //fournit un middleware Express pour activer CORS avec diverses options
 
+db.sequelize.sync()
 
+app.use(cors())
 app.use(bodyParser.json()); // pour transformer le corps de la requête en objet JS
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
